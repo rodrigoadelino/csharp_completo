e7b3622b72318ef95b86189c68c47af03880a363
Rodrigo Adelino <rodrigoadelino@outlook.com>
2024-02-22 06:57:39 -0300

    Aula16-Secao3

---
+++ Secao3/Aulas/Aula15.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+
+
+//namespace Aula15
+//{
+//    class aula15
+//    {
+//        static void Main(string[] args)
+//        {
+//            byte n0 = 126;
+//            sbyte n1 = 127;  //valor maximo que um sbyte pode receber
+//            int n2 = 1000;
+//            int n3 = 214748364; //valor maximo que um int pode receber
+//            long n4 = 214748368L; //para utilizar numeros maiores é recomendavel usar o L no final
+
+
+//            Console.WriteLine(n1);
+//            Console.WriteLine(n2);
+//            Console.WriteLine(n3);
+//            Console.WriteLine(n4);
+//        }
+//    }
+//}
---
+++ Secao3/Aulas/Aula16.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace Aula16
+{
+    class aula16
+    {
+        static void Main(string[] args)
+        {
+            bool completo = false;
+            char genero = 'F';     //para um caractere utilizar apenas aspas simples
+            char letra = '\u0041'; //utilizando unicode
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L;
+            float n5 = 4.5f;
+            double n6 = 4.5;
+            String nome = "Maria Green";
+            Object obj1 = "Alex Brown";
+            Object obj2 = 4.5f;
+
+            int n7 = int.MinValue;  //para saber o menor valor
+            int n8 = int.MaxValue;  //para saber o maior valor
+
+
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(letra);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+            Console.WriteLine(n7);
+            Console.WriteLine(n8);
+        }
+    }
+}

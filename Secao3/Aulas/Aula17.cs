f6df1152ad82a648e4e1c29aaae82f0b91f8f86b
Rodrigo Adelino <rodrigoadelino@outlook.com>
2024-02-24 08:50:36 -0300

    Aula17-Secao3

---
+++ Secao3/Aulas/Aula17.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Convenções
+// - Camel Case: lastName(parametros de métodos, variaveis dentro de métodos)
+// - Pascal Case: LastName (namespaces, classe, properties e métodos)
+// - Padrão _lastName(atributos "internos" da classe"
+
+
+namespace Curso // "Curso" namespace - Pascal Case
+{
+    class ContaBancaria  //Pascal Case
+    {
+        public string Titular { get; set; } //Titular(prpriedade) Pascal Case
+        private double _saldo;              //Atributo interno da classe
+
+        public void Deposito (double quantia) //"Deposito" Pascal Case "quantia" =parametro de metodo variavel interna CamelCase
+        {
+            _saldo += quantia;
+        }
+
+    public double GetSaldo()    // "GetSaldo" = método - PascalCase
+        {
+            return _saldo;
+        }
+    }
+}

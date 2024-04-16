c5876872091947134cf1cba82227b8c82c6c836b
Rodrigo Adelino <rodrigoadelino@outlook.com>
2024-04-16 06:41:36 -0300

    Secao3-Aula21

--- Secao3/Aulas/Aula20.cs
+++ Secao3/Aulas/Aula20.cs
@@ -1,43 +1,43 @@
-﻿using System;
+﻿//using System;
 
-namespace Aula20
-{
-    class Aula20
-    {
-        static void Main(string[] args)
-        {
+//namespace Aula20
+//{
+//    class Aula20
+//    {
+//        static void Main(string[] args)
+//        {
 
-            //float x = 4.5f;
+//            //float x = 4.5f;
 
-            //double y = x;
+//            //double y = x;
 
-            //Console.WriteLine(y);
+//            //Console.WriteLine(y);
 
-            //-------------------------------
+//            //-------------------------------
 
-            //double a;
-            //float b;
+//            //double a;
+//            //float b;
 
-            //a = 5.1;
-            ////b = a; //não irá funcionar
-            //b = (float)a; //casting
-            //Console.WriteLine(b);
+//            //a = 5.1;
+//            ////b = a; //não irá funcionar
+//            //b = (float)a; //casting
+//            //Console.WriteLine(b);
 
-            //-------------------------------
-            //double a;
-            //int b;
-            //a = 5.1;
-            ////b = a; //não irá funcionar ñ é possivel converter double para int
-            //b = (int)a; //a variavel b vai ser 5 
-            //Console.WriteLine(b);
+//            //-------------------------------
+//            //double a;
+//            //int b;
+//            //a = 5.1;
+//            ////b = a; //não irá funcionar ñ é possivel converter double para int
+//            //b = (int)a; //a variavel b vai ser 5 
+//            //Console.WriteLine(b);
 
-            int a = 5;
-            int b = 2;
+//            int a = 5;
+//            int b = 2;
 
-            double resultado = (double) a / b;
-            //double resultado = a / b;
-            Console.WriteLine(resultado);
-        }
+//            double resultado = (double) a / b;
+//            //double resultado = a / b;
+//            Console.WriteLine(resultado);
+//        }
 
-    }
-}
+//    }
+//}
